75c88fad8cada571b2a2f7aad9cb0321ce90b7c4
Nur-allhi <noorefty1@gmail.com>
2021-01-27 11:32:32 +0600

    added event listener

---
+++ script.js
@@ -0,0 +1,6 @@
+const productOneIncrease = document.getElementById("productOneIncrease");
+productOneIncrease.addEventListener("click", function () {
+  const firstProductValue = document.getElementById("firstProductValue")
+    .innerText;
+  const firstProductNumber = parseFloat(firstProductValue);
+});
