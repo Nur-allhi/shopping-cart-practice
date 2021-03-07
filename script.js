ebbf87d66bd9db6c27cf95beb65865f2fc5da993
Nur-allhi <noorefty1@gmail.com>
2021-03-07 06:54:16 +0600

    Started this project

--- script.js
+++
@@ -1,6 +0,0 @@
-const productOneIncrease = document.getElementById("productOneIncrease");
-productOneIncrease.addEventListener("click", function () {
-  const firstProductValue = document.getElementById("firstProductValue")
-    .innerText;
-  const firstProductNumber = parseFloat(firstProductValue);
-});
